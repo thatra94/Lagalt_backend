78501d2a148230000ad3a01e3955ac74b1bfbed4
ellerish <ellerish11@gmail.com>
2021-03-12 11:31:41 +0100

    start up context

---
+++ Lagalt/DB/LegaltContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lagalt.DB
+{
+    public class LegaltContext : DbContext
+    {
+        public LegaltContext(DbContextOptions<LegaltContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
