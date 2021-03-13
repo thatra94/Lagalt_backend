d9e44febc447d80f4a78417b1fbf8edd94e8867d
ellerish <ellerish11@gmail.com>
2021-03-13 14:01:20 +0100

    user wip

--- Lagalt/Models/User.cs
+++ Lagalt/Models/User.cs
@@ -14,8 +14,17 @@ namespace Lagalt.Models
         [Required]
         public string Name { get; set; }
 
+        // Reference for keycloak
+        public string UserToken { get; set; }
+
         [DataType(DataType.ImageUrl)]
         [MaxLength(2083)]
         public string ImageUrl { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        //Relationship
+        public ICollection<Skill> skills;
+
     }
 }
