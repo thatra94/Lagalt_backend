1ec653b1828eb2e4fdff748b68c63d2619a511a1
ellerish <ellerish11@gmail.com>
2021-03-13 14:39:54 +0100

    profil skill/user

---
+++ Lagalt/Profiles/SkillProfil.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Lagalt.DTOs;
+using Lagalt.Models;
+
+namespace Lagalt.Profiles
+{
+    public class SkillProfil: Profile
+    {
+        public SkillProfil()
+        {
+            CreateMap<SkillDto, Skill>().ReverseMap();
+        }
+    }
+}
