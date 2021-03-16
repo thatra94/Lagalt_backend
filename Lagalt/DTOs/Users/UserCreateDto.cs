d688eba19f1f7543b2aebf2e0f9a116f9ad0f379
ellerish <ellerish11@gmail.com>
2021-03-16 12:18:49 +0100

    post user

---
+++ Lagalt/DTOs/Users/UserCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lagalt.DTOs.Users
+{
+    public class UserCreateDto
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Description { get; set; }
+        public ICollection<SkillPostDto>? Skills { get; set; }
+    }
+}
