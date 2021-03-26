ac2068559b2cb45294130c040ebb6515e5c0ae2d
ellerish <ellerish11@gmail.com>
2021-03-26 17:42:04 +0100

    userdto

---
+++ Lagalt/DTOs/Users/UserIdDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lagalt.DTOs.Users
+{
+    public class UserIdDto
+    {
+        public string UserId { get; set; }
+    }
+}
